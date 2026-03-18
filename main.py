6b033c215b25c98ac8ac274e3eb7513309a967c3
yeni mrc <yennialgc562425@gmail.com>
2026-03-18 08:41:22 -0600

    estructura de archivos

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from pymongo import MongoClient
+from datetime import datetime
+
+app = FastAPI()
+
+# conexión MongoDB Atlas
+MONGO_URI = "mongodb+srv://esp32:esp32pass@cluster0.kqtluum.mongodb.net/iot"
+
+client = MongoClient(MONGO_URI)
+db = client.iot
+collection = db.sensores
+
+@app.get("/")
+def root():
+    return {"mensaje":"API funcionando"}
+
+@app.post("/sensor")
+def guardar_sensor(data: dict):
+
+    data["fecha"] = datetime.now()
+
+    collection.insert_one(data)
+
+    return {"status":"dato guardado"}
